198bfd219780597c583ef6dc575f9c7ed7091608
Jeffrey Russom <jeffreyrussom@gmail.com>
2018-11-02 00:30:17 -0500

    Some cleanup

--- src/App.js
+++ src/App.js
@@ -4,36 +4,6 @@ import RecipesAppBar from "./RecipesAppBar/RecipesAppBar";
 
 import { withStyles } from "@material-ui/core/styles";
 
-class LambdaDemo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { loading: false, msg: null };
-    }
-
-    handleClick = e => {
-        e.preventDefault();
-
-        this.setState({ loading: true });
-        fetch("/.netlify/functions/hello")
-            .then(response => response.json())
-            .then(json => this.setState({ loading: false, msg: json.msg }));
-    };
-
-    render() {
-        const { loading, msg } = this.state;
-
-        return (
-            <p>
-                <button onClick={this.handleClick}>
-                    {loading ? "Loading..." : "Call Lambda"}
-                </button>
-                <br />
-                <span>{msg}</span>
-            </p>
-        );
-    }
-}
-
 const styles = {
     app: {
         backgroundColor: "#e6e6e6"
