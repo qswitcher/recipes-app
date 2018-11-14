cd29649d766962b35ebf8bef274920588f02403d
Jeffrey Russom <jeffreyrussom@gmail.com>
2018-11-13 22:16:14 -0600

    Make stuff pretty

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -1,6 +1,9 @@
 import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import CheckList from "../CheckList/CheckList";
 
 const getRecipe = gql`
     query GetRecipe($id: String!) {
@@ -9,12 +12,32 @@ const getRecipe = gql`
             photo
             title
             description
+            directions
+            ingredients
+            photo
+            prepTime
+            cookTime
+            readyIn
+            recipeYield
         }
     }
 `;
 
+const styles = theme => ({
+    // btnCreate: {
+    //     marginRight: theme.spacing.unit
+    // },
+    // btnCancel: {
+    //     marginRight: theme.spacing.unit
+    // }
+    recipeImg: {
+        width: "100%"
+    }
+});
+
 class Recipe extends React.Component {
     render() {
+        const { classes } = this.props;
         const { id } = this.props.match.params;
         return (
             <Query query={getRecipe} variables={{ id }}>
@@ -23,11 +46,29 @@ class Recipe extends React.Component {
                         return null;
                     }
                     const recipe = data.getRecipe;
-                    return <p>{recipe.title}</p>;
+                    return (
+                        <Grid container spacing={24}>
+                            <Grid item md={6}>
+                                <img
+                                    className={classes.recipeImg}
+                                    alt="food"
+                                    src={recipe.photo}
+                                />
+                            </Grid>
+                            <Grid item md={6}>
+                                <h1>{recipe.title}</h1>
+                                <div>{recipe.description}</div>
+                                <h2>Ingredients</h2>
+                                <CheckList blob={recipe.ingredients} />
+                                <h2>Directions</h2>
+                                <CheckList blob={recipe.directions} />
+                            </Grid>
+                        </Grid>
+                    );
                 }}
             </Query>
         );
     }
 }
 
-export default Recipe;
+export default withStyles(styles)(Recipe);
